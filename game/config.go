0d16ce3f872b5f16ee6623887b71a2cf19032bc8
Dave O <davidobrite@gmail.com>
2014-08-07 05:59:14 -0700

    exposed chunk variables

--- game/config.go
+++ game/config.go
@@ -4,8 +4,8 @@ import ()
 
 const (
 	seed              = 0xDEADBEEF
-	Chunk_x           = 16
-	Chunk_y           = 16
+	chunk_x           = 16
+	chunk_y           = 16
 	world_x           = 3 // in chunks
 	world_y           = 3 // in chunks
 	depth             = 32
@@ -17,5 +17,7 @@ type wireConfig struct {
 	Event   string `json:"event"`
 	Chunk_x int    `json:"chunk_x"`
 	Chunk_y int    `json:"chunk_y"`
+	World_x int    `json:"world_x"`
+	World_y int    `json:"world_y"`
 	Id      string `json:"id"`
 }
