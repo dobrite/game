7f665c6c6e2ff9f58e756d537b2d80569efbb92c
Dave O <davidobrite@gmail.com>
2014-08-13 06:26:12 -0700

    fixed issue with movement

--- game/positions.go
+++ game/positions.go
@@ -32,22 +32,22 @@ func (p *position) toWorldCoords() *worldCoords {
 }
 
 func (p *position) move(z, x int) {
-	if p.x+x > chunkX {
+	if p.x+x >= chunkX {
 		p.cx = p.cx + 1
 		p.x = 0
 	} else if p.x+x < 0 {
 		p.cx = p.cx - 1
-		p.x = chunkX
+		p.x = chunkX - 1
 	} else {
 		p.x = p.x + x
 	}
 
-	if p.z+z > chunkZ {
+	if p.z+z >= chunkZ {
 		p.cz = p.cz + 1
 		p.z = 0
 	} else if p.z+z < 0 {
 		p.cz = p.cz - 1
-		p.z = chunkZ
+		p.z = chunkZ - 1
 	} else {
 		p.z = p.z + z
 	}
